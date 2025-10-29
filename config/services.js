e3584b673f55358895c6d21013cc75f40fa599f1
Ennimex <alexanderhernan325@gmail.com>
2025-10-29 13:29:08 -0600

    Reset de los servicios

--- config/services.js
+++ config/services.js
@@ -1,14 +1,13 @@
 const nodemailer = require('nodemailer');
 const twilio = require('twilio');
 
+// Configuración de Email
 const emailTransporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465, 
-    secure: true, // Debe ser 'true' para el puerto 465 (TLS implícito)
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD // Esto ya es la App Password, ¡correcto!
-    }
+  service: 'gmail', // o el servicio que prefieras
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
 });
 
 // Configuración de Twilio para SMS
